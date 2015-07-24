7b8ad28c62d8c5125abd338faf137f8f181b486a
spoofy <spoofy@exploitscience.org>
2015-07-24 11:26:59 -0700

    packaging added

---
+++ ALSales/Setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='AlertLogic',
+      version='0.1',
+      description='Python API Wrappers for Alert Logic API',
+      url='https://github.com/SpoofyRoot/AlertLogic/',
+      author='Johnathan Norman',
+      author_email='jnorman@alertlogic.com',
+      license='MIT',
+      packages=['AlertLogic'],
+      zip_safe=False)
